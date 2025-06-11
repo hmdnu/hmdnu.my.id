68709aadb4b90d694bfd3ad2cc5fcad4c433d4fd
hmdnu <daniuyan71@gmail.com>
2025-06-11 20:20:29 +0700

    feat: about

---
+++ src/data/education.ts
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    logo: "/src/img/polinema.png",
+    institution: "State Polytechnic of Malang",
+    title: "Bachelor of Applied Science, Information Technology",
+    date: "August 2023 - Present",
+  },
+  {
+    logo: "/src/img/smarifda.png",
+    institution: "Maarif NU Senior High School, Pandaan",
+    title: "Social Science",
+    date: "July 2020 - June 2023",
+  },
+] as const;
