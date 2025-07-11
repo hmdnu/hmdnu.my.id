609d87ee8c21d775270cb1c48ce5b15616e6460b
hmdnu <daniuyan71@gmail.com>
2025-07-11 15:18:48 +0700

    wip: add theme switcher component with local storage persistence

---
+++ src/components/Theme.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { icon } from "@/constants/icon";
+    import { onMount } from "svelte";
+
+    let theme = "dark";
+
+    onMount(() => {
+        theme = localStorage.getItem("theme") || "dark";
+        document.documentElement.setAttribute("data-theme", theme);
+    });
+
+    function themeSwitcher() {
+        theme = theme === "dark" ? "light" : "dark";
+        localStorage.setItem("theme", theme);
+        document.documentElement.setAttribute("data-theme", theme);
+    }
+</script>
+
+<button
+    on:click={themeSwitcher}
+    class={`${theme === "dark" ? icon.lightMode : icon.darkMode} size-5 text-primary hover:cursor-pointer`}
+    aria-label="theme-button"
+></button>
