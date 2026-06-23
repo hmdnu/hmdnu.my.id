e334ef687abe76c6fd7c62c55ac8e6206caee98b
hmdnu <daniuyan71@gmail.com>
2026-06-23 08:38:43 +0700

    Update organization date

--- src/constants/organization.ts
+++ src/constants/organization.ts
@@ -3,12 +3,12 @@ export const organizations = [
     logo: "/assets/wri.png",
     name: "Workshop Riset Informatika",
     position: "Vice President of Frontend Development Departement",
-    date: "January 2025 - Present",
+    date: "January 2025 - January 2026",
   },
   {
     logo: "/assets/wri.png",
     name: "Workshop Riset Informatika",
     position: "Frontend Web Development Mentor",
-    date: "October 2024 - Present",
+    date: "September 2024 - January 2026",
   },
 ] as const;
